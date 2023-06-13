0da72b840c7249b0293d6b0ec575c7eefad0f891
lindsayRae <lbarnett712@gmial.com>
2023-06-13 16:42:04 -0400

    added new favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,32 +1,36 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class SandBox extends Document {
   render() {
-    return <Html lang="en">
+    return (
+      <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="/img/favicon.png" type="image/png" />
+          {/* <link rel="shortcut icon" href="/img/favicon-32x32.png" type="image/png" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+          <link rel="manifest" href="/img/site.webmanifest" />
         </Head>
 
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>;
+      </Html>
+    );
   }
-
 } // ======================================================================
 
-SandBox.getInitialProps = async ctx => {
+SandBox.getInitialProps = async (ctx) => {
   const originalRenderPage = ctx.renderPage; // Run the React rendering logic synchronously
 
-  ctx.renderPage = () => originalRenderPage({
-    // Useful for wrapping the whole react tree
-    enhanceApp: App => App,
-    // Useful for wrapping in a per-page basis
-    enhanceComponent: Component => Component
-  }); // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // Useful for wrapping the whole react tree
+      enhanceApp: (App) => App,
+      // Useful for wrapping in a per-page basis
+      enhanceComponent: (Component) => Component
+    }); // Run the parent `getInitialProps`, it now includes the custom `renderPage`
 
   const initialProps = await Document.getInitialProps(ctx);
-  return { ...initialProps
-  };
+  return { ...initialProps };
 };
