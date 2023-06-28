32516732ecb83b22df851b9d3130e062b9f7c8c3
lindsayRae <lbarnett712@gmial.com>
2023-06-28 08:40:51 -0400

    updated alt text

--- src/components/reuseable/FigureImage.jsx
+++ src/components/reuseable/FigureImage.jsx
@@ -1,20 +1,22 @@
 import Image from 'next/image';
 
 // ====================================================
-const FigureImage = props => {
-  const {
-    className,
-    src,
-    width,
-    height,
-    ...others
-  } = props;
-  return <figure className={className} {...others}>
-      <Image width={width} height={height} src={src} alt="demo" style={{
-      width: '100%',
-      height: 'auto'
-    }} />
-    </figure>;
+const FigureImage = (props) => {
+  const { className, src, width, height, ...others } = props;
+  return (
+    <figure className={className} {...others}>
+      <Image
+        width={width}
+        height={height}
+        src={src}
+        alt=""
+        style={{
+          width: '100%',
+          height: 'auto'
+        }}
+      />
+    </figure>
+  );
 };
 
 export default FigureImage;
