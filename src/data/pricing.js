9c2c33359c6d123182365abf6cab9e98334e0485
lindsayRae <lbarnett712@gmial.com>
2023-07-30 13:33:09 -0400

    removed self pay discount pricing

--- src/data/pricing.js
+++ src/data/pricing.js
@@ -4,7 +4,6 @@ export const pricingList1 = [
     features: [
       '60 minutes - $300',
       '90 minutes - $350',
-      '15% self pay discount',
       '---------------------',
       'This is a time for me to get to know you. We will discuss your concerns, review your medical and psychiatric history, social history, and developmental history. This is a longer visit which can take 60-90 minutes.'
     ]
@@ -14,7 +13,6 @@ export const pricingList1 = [
     features: [
       '15 minutes - $150',
       '30 minutes - $200',
-      '50% self pay discount',
       '---------------------',
       'This is a shorter visit to check in, see how you are tolerating medication if you are taking any, how you are functioning including energy, sleep, appetite, etc. These sessions can include short supportive therapy and/or lifestyle coaching which are important part of mental health.'
     ]
