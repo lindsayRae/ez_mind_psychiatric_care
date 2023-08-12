a90d16848e2aeadd71769896cd963ca82d2993e7
lindsayRae <lbarnett712@gmial.com>
2023-08-12 18:24:05 -0400

    changed display class

--- src/components/blocks/hero/Hero4.jsx
+++ src/components/blocks/hero/Hero4.jsx
@@ -10,8 +10,10 @@ const Hero4 = () => {
         //   backgroundImage: 'url(/img/photos/hero_2000x1692.webp)'
         // }}
         id="heroImage"
-        className="rounded-4-lg-start col-lg-6 order-lg-2 position-lg-absolute top-0 end-0 image-wrapper bg-image bg-cover h-100 min-vh-50"
+        className="rounded-4-lg-start col-lg-6 order-lg-2 position-lg-absolute top-0 end-0 image-wrapper bg-image bg-cover h-100 min-vh-50 "
       >
+        {/* Add below class if want to hide image on mobile */}
+        {/* d-none d-sm-block */}
         {/* <Image src="/img/photos/hero_2000x1692.webp" layout="fill" objectFit="cover" className="rounded-4-lg-start" /> */}
       </div>
 
@@ -19,8 +21,8 @@ const Hero4 = () => {
         <div className="row">
           <div className="col-lg-6">
             <div className="mt-10 mt-md-11 mt-lg-n10 px-10 px-md-11 ps-lg-0 pe-lg-13 text-center text-lg-start">
-              <h1 className="display-1 mb-5" style={slideInDownAnimate('600ms')}>
-                Making Mental Health Care Easily Accessible While Providing Quality Holistic Care.
+              <h1 className="display-2 mb-5" style={slideInDownAnimate('600ms')}>
+                Making Mental Health Care Easily Accessible While Providing Quality Holistic Care
               </h1>
               <p className="lead fs-25 lh-sm mb-7 pe-md-10" style={slideInDownAnimate('900ms')}>
                 Providing care to 18+ from the comfort of your own home.
